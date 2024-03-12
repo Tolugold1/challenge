316e616a0e13ad9bc19a6cf057ed00f28050cf29
Tolulope Adeleke <toluadelekesteve@gmail.com>
2024-03-12 09:09:13 +0100

    Update signin.js

--- src/Components/Signup/signin.js
+++ src/Components/Signup/signin.js
@@ -71,9 +71,9 @@ const SignIn = () => {
             if (resp.success === true) { // authenticate and search if user has filled in the details form, if user details found, redirect to dashboard else redirect to details form page.
                 setRespo(true);
                 localStorage.setItem("userGitHubAcct", resp.status[0].githubname)
-                window.location.assign("https://challenge-umber-six.vercel.app/dashboard")
+                window.location.assign("https://challengedeveloper.vercel.app/dashboard")
             } else {
-                window.location.assign("https://challenge-umber-six.vercel.app/details")
+                window.location.assign("https://challengedeveloper.vercel.app/details")
             }
         })
     }
